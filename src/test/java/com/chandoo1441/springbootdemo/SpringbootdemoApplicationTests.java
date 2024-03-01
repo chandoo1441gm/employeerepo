3254cd9b91300b4f227fbd03aca13e3d6e867a06
chandooiitm <chandoo_iitm@yahoo.co.in>
2024-03-01 13:15:14 +0530

    Init commit

---
+++ src/test/java/com/chandoo1441/springbootdemo/SpringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandoo1441.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
